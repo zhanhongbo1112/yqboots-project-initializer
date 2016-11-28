5a35529e59a2930aa9a2507253bef1256bff72aa
zhanhongbo1112 <eric.zhan@163.com>
2016-11-28 21:56:19 +0800

    updates templates

--- src/main/resources/META-INF/resources/dojo/app/projects/initializer/form.js
+++ src/main/resources/META-INF/resources/dojo/app/projects/initializer/form.js
@@ -1,7 +1,18 @@
-define([], function () {
+define(['dojo/_base/lang'], function (lang) {
     return {
         startup: function () {
+            $('#file').change(lang.hitch(this, '_onChangeFile'));
+            $('#cancel').click(lang.hitch(this, '_onCancel'));
+        },
 
+        _onChangeFile : function(e) {
+            var inputFile = $(e.target);
+            inputFile.parent().next().val(inputFile.val());
+        },
+
+        _onCancel: function (e) {
+            e.preventDefault();
+            window.history.back();
         }
     };
 });
