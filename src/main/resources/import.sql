ab60427938678eed72ebb87d5d84ba515dbb36fc
zhanhongbo1112 <eric.zhan@163.com>
2016-12-01 22:17:16 +0800

    bug fixed for domain sheet

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,17 @@
 INSERT INTO PRJ_MENUITEM (NAME, URL, MENU_GROUP, MENU_ITEM_GROUP) VALUES ('PROJECTS_INITIALIZER', '/projects/initializer', 'PROJECTS', 'OPEN_SOURCE');
+
+insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_SKIN', 'Light', 'LIGHT', '');
+insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_SKIN', 'Dark', 'DARK', '');
+
+insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR', 'Green', 'GREEN', '');
+insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR', 'Blue', 'BLUE', '');
+insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR', 'Orange', 'ORANGE', '');
+insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR', 'Red', 'RED', '');
+insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR', 'Light', 'LIGHT', '');
+insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR', 'Purple', 'PURPLE', '');
+insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR', 'Aqua', 'AQUA', '');
+insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR', 'Brown', 'BROWN', '');
+insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR', 'Dark Blue', 'DARK_BLUE', '');
+insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR', 'Light Green', 'LIGHT_GREEN', '');
+insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR', 'Dark Red', 'DARK_RED', '');
+insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR', 'Teal', 'TEAL', '');
