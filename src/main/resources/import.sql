7353b21e336551bd027a2b805da56f6893c4c83a
zhanhongbo1112 <eric.zhan@163.com>
2016-12-12 22:21:08 +0800

    support i18n of data dict

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,17 +1,31 @@
 INSERT INTO PRJ_MENUITEM (NAME, URL, MENU_GROUP, MENU_ITEM_GROUP) VALUES ('PROJECTS_INITIALIZER', '/projects/initializer', 'PROJECTS', 'OPEN_SOURCE');
 
 insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_SKIN', 'Light', 'LIGHT', '');
+insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_SKIN_zh_CN', '淡色调', 'LIGHT', '');
 insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_SKIN', 'Dark', 'DARK', '');
+insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_SKIN_zh_CN', '暗色调', 'DARK', '');
 
 insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR', 'Green', 'GREEN', '');
+insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR_zh_CN', '绿色', 'GREEN', '');
 insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR', 'Blue', 'BLUE', '');
+insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR_zh_CN', '蓝色', 'BLUE', '');
 insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR', 'Orange', 'ORANGE', '');
+insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR_zh_CN', '橙色', 'ORANGE', '');
 insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR', 'Red', 'RED', '');
+insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR_zh_CN', '红色', 'RED', '');
 insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR', 'Light', 'LIGHT', '');
+insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR_zh_CN', '淡色', 'LIGHT', '');
 insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR', 'Purple', 'PURPLE', '');
+insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR_zh_CN', '紫色', 'PURPLE', '');
 insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR', 'Aqua', 'AQUA', '');
+insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR_zh_CN', '水绿色', 'AQUA', '');
 insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR', 'Brown', 'BROWN', '');
+insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR_zh_CN', '棕色', 'BROWN', '');
 insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR', 'Dark Blue', 'DARK_BLUE', '');
+insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR_zh_CN', '深蓝色', 'DARK_BLUE', '');
 insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR', 'Light Green', 'LIGHT_GREEN', '');
+insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR_zh_CN', '浅绿色', 'LIGHT_GREEN', '');
 insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR', 'Dark Red', 'DARK_RED', '');
+insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR_zh_CN', '暗红色', 'DARK_RED', '');
 insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR', 'Teal', 'TEAL', '');
+insert into PRJ_DATA_DICT (name, text, value, description) values ('THEME_COLOR_zh_CN', '鸭绿色', 'TEAL', '');
